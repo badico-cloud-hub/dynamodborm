4b4583a9961ac9b35b318118d2ff3c15cca5f572
lgsantana1 <lgsantana1@latam.stefanini.com>
2018-11-18 12:16:33 -0200

    overhide common methods done

--- __tests__/helpers/appendCustomMethods.spec.js
+++ __tests__/helpers/appendCustomMethods.spec.js
@@ -1,75 +1,110 @@
-import { applyCommonMethods } from '../../src/helpers/applyCommonMethods'
+import { appendCustomMethods } from '../../src/helpers/appendCustomMethods'
+import applyCommonMethods from '../../src/helpers/applyCommonMethods'
 
-describe('applyCommonMethods in Helpers, applied into every class that represents a entity on the ORM', () => {
-  it('should have present all commom methods', () => {
-    const schema = () => ({
-      name: {
-        type: 'String'
-      }
-    })
-
-    const data = {
-      foo: 'a foo data'
+describe('appendCustomMethods in Helpers, avaliable to apply custom functions as methods to every class that represents a entity on the ORM', () => {
+  it('should have present all custom methods applyed', () => {
+    class Foo {
     }
 
-    const retrievedData = parseFields(schema)(data)
-    expect(Object.keys(retrievedData)).toHaveLength(0)
+    const customMethod = {
+      baz: function() {}
+    }
+    applyCommonMethods(Foo)
+    appendCustomMethods(Foo, customMethod)
+    const foo = new Foo()
+    expect(foo).toHaveProperty('addItem')
+    expect(foo).toHaveProperty('delete')
+    expect(foo).toHaveProperty('get')
+    expect(foo).toHaveProperty('getItem')
+    expect(foo).toHaveProperty('removeItem')
+    expect(foo).toHaveProperty('save')
+    expect(foo).toHaveProperty('set')
+    expect(foo).toHaveProperty('update')
+    expect(foo).toHaveProperty('updateItem')
+    expect(foo).toHaveProperty('validate')
+
+    expect(foo).toHaveProperty('baz')
   })
 
-  it('should put a _ as prefix if the target class already use that method name', () => {
-    const schema = () => ({
-      name: {
-        type: 'String'
-      }
-    })
-
-    const data = {
-      foo: 'a foo data',
-      name: 'a foo name'
+  it('should put a _ prefix in the overhide version of the method', () => {
+    class Foo {
     }
+    applyCommonMethods(Foo)
 
-    const retrievedData = parseFields(schema)(data)
-    expect(Object.keys(retrievedData)).toHaveLength(1)
-    expect(retrievedData).toHaveProperty('name', data.name)
+    const customMethod = {
+      baz: function() {},
+      addItem: function() {},
+      delete: function() {},
+      get: function() {},
+      getItem: function() {},
+      removeItem: function() {},
+      save: function() {},
+      set: function() {},
+      update: function() {},
+      updateItem: function() {},
+      validate: function() {},
+    }
+    appendCustomMethods(Foo, customMethod)
+    const foo = new Foo()
+    expect(foo).toHaveProperty('addItem')
+    expect(foo).toHaveProperty('delete')
+    expect(foo).toHaveProperty('get')
+    expect(foo).toHaveProperty('getItem')
+    expect(foo).toHaveProperty('removeItem')
+    expect(foo).toHaveProperty('save')
+    expect(foo).toHaveProperty('set')
+    expect(foo).toHaveProperty('update')
+    expect(foo).toHaveProperty('updateItem')
+    expect(foo).toHaveProperty('validate')
+
+    expect(foo).toHaveProperty('baz')
+    expect(foo).toHaveProperty('_addItem')
+    expect(foo).toHaveProperty('_delete')
+    expect(foo).toHaveProperty('_get')
+    expect(foo).toHaveProperty('_getItem')
+    expect(foo).toHaveProperty('_removeItem')
+    expect(foo).toHaveProperty('_save')
+    expect(foo).toHaveProperty('_set')
+    expect(foo).toHaveProperty('_update')
+    expect(foo).toHaveProperty('_updateItem')
+    expect(foo).toHaveProperty('_validate')
   })
 
   it('should the prefix decision be individual for each method', () => {
-    const root = () => ({
-      name: {
-        type: 'String'
-      },
-      email: {
-        type: 'String'
-        // validator: hasToBe.string().email() // .required()
-      },
-      foo: (embed, Class) => ({
-        type: 'Collection',
-        menberType: embed(Class)
-      })
-    })
-
-    const objectValue = {
-      schema: () => ({
-        bar: {
-          type: 'Name'
-        }
-      }),
-      key: 'foo'
+    class Foo {
     }
+    applyCommonMethods(Foo)
 
-    const data = {
-      foo: {
-        bar: 1,
-        baz: 'a baz data'
-      },
-      name: 'a foo name',
-      email: 'teste'
+
+    const customMethod = {
+      baz: function() {},
+      addItem: function() {},
+      delete: function() {},
+      get: function() {},
+      getItem: function() {},
+      removeItem: function() {},
+      save: function() {},
     }
+    appendCustomMethods(Foo, customMethod)
+    const foo = new Foo()
+    expect(foo).toHaveProperty('addItem')
+    expect(foo).toHaveProperty('delete')
+    expect(foo).toHaveProperty('get')
+    expect(foo).toHaveProperty('getItem')
+    expect(foo).toHaveProperty('removeItem')
+    expect(foo).toHaveProperty('save')
 
-    const retrievedData = parseFields(root, objectValue)(data)
-    expect(Object.keys(retrievedData)).toHaveLength(3)
-    expect(retrievedData).toHaveProperty('name', data.name)
-    expect(retrievedData.foo).toHaveProperty('bar', data.foo.bar)
-    expect(retrievedData.foo).not.toHaveProperty('baz')
+    expect(foo).toHaveProperty('set')
+    expect(foo).toHaveProperty('update')
+    expect(foo).toHaveProperty('updateItem')
+    expect(foo).toHaveProperty('validate')
+
+    expect(foo).toHaveProperty('baz')
+    expect(foo).toHaveProperty('_addItem')
+    expect(foo).toHaveProperty('_delete')
+    expect(foo).toHaveProperty('_get')
+    expect(foo).toHaveProperty('_getItem')
+    expect(foo).toHaveProperty('_removeItem')
+    expect(foo).toHaveProperty('_save')
   })
 })
--- __tests__/helpers/applyCommonMethods.spec.js
+++ __tests__/helpers/applyCommonMethods.spec.js
@@ -1,75 +1,87 @@
-import { appendCustomMethods } from '../../src/helpers/appendCustomMethods'
+import applyCommonMethods from '../../src/helpers/applyCommonMethods'
 
-describe('appendCustomMethods in Helpers, avaliable to apply custom functions as methods to every class that represents a entity on the ORM', () => {
-  it('should have present all custom methods applyed', () => {
-    const schema = () => ({
-      name: {
-        type: 'String'
-      }
-    })
-
-    const data = {
-      foo: 'a foo data'
-    }
-
-    const retrievedData = parseFields(schema)(data)
-    expect(Object.keys(retrievedData)).toHaveLength(0)
+describe('applyCommonMethods in Helpers, applied into every class that represents a entity on the ORM', () => {
+  it('should have present all commom methods', () => {
+    class Foo {}
+    applyCommonMethods(Foo)
+    const foo = new Foo()
+    expect(foo).toHaveProperty('addItem')
+    expect(foo).toHaveProperty('delete')
+    expect(foo).toHaveProperty('get')
+    expect(foo).toHaveProperty('getItem')
+    expect(foo).toHaveProperty('removeItem')
+    expect(foo).toHaveProperty('save')
+    expect(foo).toHaveProperty('set')
+    expect(foo).toHaveProperty('update')
+    expect(foo).toHaveProperty('updateItem')
+    expect(foo).toHaveProperty('validate')
   })
 
-  it('should put a _ prefix in the overhide version of the method', () => {
-    const schema = () => ({
-      name: {
-        type: 'String'
-      }
-    })
-
-    const data = {
-      foo: 'a foo data',
-      name: 'a foo name'
+  it('should put a _ as prefix if the target class already use that method name', () => {
+    class Foo {
+      addItem(){}
+      delete(){}
+      get(){}
+      getItem(){}
+      removeItem(){}
+      save(){}
+      set(){}
+      update(){}
+      updateItem(){}
+      validate(){}
     }
+    applyCommonMethods(Foo)
+    const foo = new Foo()
+    expect(foo).toHaveProperty('addItem')
+    expect(foo).toHaveProperty('delete')
+    expect(foo).toHaveProperty('get')
+    expect(foo).toHaveProperty('getItem')
+    expect(foo).toHaveProperty('removeItem')
+    expect(foo).toHaveProperty('save')
+    expect(foo).toHaveProperty('set')
+    expect(foo).toHaveProperty('update')
+    expect(foo).toHaveProperty('updateItem')
+    expect(foo).toHaveProperty('validate')
 
-    const retrievedData = parseFields(schema)(data)
-    expect(Object.keys(retrievedData)).toHaveLength(1)
-    expect(retrievedData).toHaveProperty('name', data.name)
+    expect(foo).toHaveProperty('_addItem')
+    expect(foo).toHaveProperty('_delete')
+    expect(foo).toHaveProperty('_get')
+    expect(foo).toHaveProperty('_getItem')
+    expect(foo).toHaveProperty('_removeItem')
+    expect(foo).toHaveProperty('_save')
+    expect(foo).toHaveProperty('_set')
+    expect(foo).toHaveProperty('_update')
+    expect(foo).toHaveProperty('_updateItem')
+    expect(foo).toHaveProperty('_validate')
   })
 
   it('should the prefix decision be individual for each method', () => {
-    const root = () => ({
-      name: {
-        type: 'String'
-      },
-      email: {
-        type: 'String'
-        // validator: hasToBe.string().email() // .required()
-      },
-      foo: (embed, Class) => ({
-        type: 'Collection',
-        menberType: embed(Class)
-      })
-    })
-
-    const objectValue = {
-      schema: () => ({
-        bar: {
-          type: 'Name'
-        }
-      }),
-      key: 'foo'
+    class Foo {
+      addItem(){}
+      delete(){}
+      get(){}
+      getItem(){}
+      removeItem(){}
+      save(){}
     }
+    applyCommonMethods(Foo)
+    const foo = new Foo()
+    expect(foo).toHaveProperty('addItem')
+    expect(foo).toHaveProperty('delete')
+    expect(foo).toHaveProperty('get')
+    expect(foo).toHaveProperty('getItem')
+    expect(foo).toHaveProperty('removeItem')
+    expect(foo).toHaveProperty('save')
+    expect(foo).toHaveProperty('set')
+    expect(foo).toHaveProperty('update')
+    expect(foo).toHaveProperty('updateItem')
+    expect(foo).toHaveProperty('validate')
 
-    const data = {
-      foo: {
-        bar: 1,
-        baz: 'a baz data'
-      },
-      name: 'a foo name',
-      email: 'teste'
-    }
-
-    const retrievedData = parseFields(root, objectValue)(data)
-    expect(Object.keys(retrievedData)).toHaveLength(3)
-    expect(retrievedData).toHaveProperty('name', data.name)
-    expect(retrievedData.foo).toHaveProperty('bar', data.foo.bar)
-    expect(retrievedData.foo).not.toHaveProperty('baz')
+    expect(foo).toHaveProperty('_addItem')
+    expect(foo).toHaveProperty('_delete')
+    expect(foo).toHaveProperty('_get')
+    expect(foo).toHaveProperty('_getItem')
+    expect(foo).toHaveProperty('_removeItem')
+    expect(foo).toHaveProperty('_save')
   })
 })
