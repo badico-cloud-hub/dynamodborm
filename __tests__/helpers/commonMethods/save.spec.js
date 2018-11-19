c48da669785ea521f7e71468b846c1dac7f31971
lgsantana1 <lgsantana1@latam.stefanini.com>
2018-11-19 21:01:42 -0200

    duplication removed from save.spec

--- __tests__/helpers/commonMethods/save.spec.js
+++ __tests__/helpers/commonMethods/save.spec.js
@@ -1,9 +1,10 @@
 import { save } from '../../../src/helpers/commonMethods/save'
 
 describe('save commom method', () => {
-    it('should use the connection update method', async(done) => {
-        expect.assertions(1)
-        const mock = {
+    let mock
+
+    beforeEach(() => {
+        mock = {
             get: function() {
                 return this
             },
@@ -11,7 +12,9 @@ describe('save commom method', () => {
                 update: jest.fn((obj) => Promise.resolve(obj))
             }
         }
-
+    })
+    it('should use the connection update method', async(done) => {
+        expect.assertions(1)
         const func = save.bind(mock)
         await func()
         expect(mock.connection.update).toHaveBeenCalled()
@@ -20,14 +23,7 @@ describe('save commom method', () => {
 
     it('should handle createdAt and updatedAt fields in a object without id', async(done) => {
         expect.assertions(3)
-        const mock = {
-            get: function() {
-                return this
-            },
-            connection: {
-                update: jest.fn((obj) => Promise.resolve(obj))
-            }
-        }
+
         const func = save.bind(mock)
         await func()
         expect(mock).toHaveProperty('createdAt')
@@ -38,17 +34,10 @@ describe('save commom method', () => {
 
     it('should handle updatedAt field in a object with id', async(done) => {
         expect.assertions(3)
-        const mock = {
-            id: 'mockId',
+        Object.assign(mock, {
             createdAt: '2018-11-18T16:13:52.863Z',
             updatedAt: '2018-11-18T16:13:52.863Z',
-            get: function() {
-                return this
-            },
-            connection: {
-                update: jest.fn((obj) => Promise.resolve(obj))
-            }
-        }
+        })
         const func = save.bind(mock)
         await func()
         expect(mock).toHaveProperty('createdAt')
@@ -59,17 +48,10 @@ describe('save commom method', () => {
 
     it('should return the same instance of the object', async(done) => {
         expect.assertions(1)
-        const mock = {
-            id: 'mockId',
+        Object.assign(mock, {
             createdAt: '2018-11-18T16:13:52.863Z',
             updatedAt: '2018-11-18T16:13:52.863Z',
-            get: function() {
-                return this
-            },
-            connection: {
-                update: jest.fn((obj) => Promise.resolve(obj))
-            }
-        }
+        })
         const func = save.bind(mock)
         const sameAsMock = await func()
         expect(sameAsMock).toBe(mock)
