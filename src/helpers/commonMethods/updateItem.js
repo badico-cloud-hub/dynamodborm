ca67dd4b06e7c24ddb44d9815f83eee557fc8c86
lgsantana1 <lgsantana1@latam.stefanini.com>
2018-11-19 21:48:38 -0200

    removed garbage left behind

--- src/helpers/commonMethods/updateItem.js
+++ src/helpers/commonMethods/updateItem.js
@@ -3,15 +3,6 @@ import { throwIfIsInvalidList } from './throwIfIsInvalidList'
 
 export function updateItem(itemKey, Item) {
   throwIfIsInvalidList(this[itemKey])
-
-  if (isInvalidList(this[itemKey])) {
-    throw new DomainError({
-      error: new Error('The item is not a list'),
-      args: [itemKey, Item],
-      method: 'updateItem'
-    }, 'NotValidOperation')
-  }
-  
   let found = false
   this[itemKey] = this[itemKey].map(
     (item) => item.id === Item.id ? (found = true, Item) : item
