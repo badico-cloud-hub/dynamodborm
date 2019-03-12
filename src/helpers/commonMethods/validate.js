6b7ae05a3fe69486c774f9c1e11125d9d7726f17
lgsantana1 <lgsantana1@latam.stefanini.com>
2019-03-12 11:46:50 -0300

    new joi callback schema

--- src/helpers/commonMethods/validate.js
+++ src/helpers/commonMethods/validate.js
@@ -2,33 +2,32 @@ import { DynamoDBORMError } from '../../DynamoDBORMError'
 
 function validator(joi, values, schema, self) {
   console.log('###args joi, values, schema, self', joi, values, schema, self)
-  return new Promise((resolve, reject) => {
-    const a = joi(values, schema, { abortEarly: false })
-    console.log('###', a)
-    if (a.error) {
-      const validationError = new DynamoDBORMError(
-        {
-          error,
-          method: 'validate',
-          args: [ joi, values, schema, self ],
-          className: 'Model'
-        },
-        'ValidationError',
-      )
-      
-     const errors =  error.details.map(error => {
-        const identifier = error.path.filter((p) => isNaN(p)).join('.') 
-        const { message } = error
-        return new DynamoDBORMError({
-          method: 'validate',
-          args: [ joi, values, schema, self ],
-          className: 'Model'
-        }, identifier, message)
-      })
-      return reject(DynamoDBORMError.fromArray(errors, 'ValidationError'))
-    }
-    return resolve(self)
-  })
+  return new Promise((resolve, reject) => joi(values, schema, { abortEarly: false }, (error) => {
+      if (error) {
+        const validationError = new DynamoDBORMError(
+          {
+            error,
+            method: 'validate',
+            args: [ joi, values, schema, self ],
+            className: 'Model'
+          },
+          'ValidationError',
+        )
+        
+       const errors =  error.details.map(error => {
+          const identifier = error.path.filter((p) => isNaN(p)).join('.') 
+          const { message } = error
+          return new DynamoDBORMError({
+            method: 'validate',
+            args: [ joi, values, schema, self ],
+            className: 'Model'
+          }, identifier, message)
+        })
+        return reject(DynamoDBORMError.fromArray(errors, 'ValidationError'))
+      }
+      return resolve(self)
+    })
+  )
 }
 
 export function validate() {
